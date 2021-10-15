09235f7af2200109afaff6a38596741e88bbd1cc
putriayusushmita <putriayusushmita6@gmail.com>
2021-10-14 22:03:59 -0700

    first commit

---
+++ Perulangan.java
@@ -0,0 +1,18 @@
+public class Perulangan {
+    public static void main(String[] args){
+
+        for (int i = 6, k = 0; i > 0 && k < 6; i--, k++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < k; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j < k; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
